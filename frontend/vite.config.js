060af5cdcd11ab6e41c78384e2538b9ed7c7dc9b
Vansh Thapar <vanshthapar.professional@gmail.com>
2025-07-28 18:17:56 +0530

    changed vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,28 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss(), ],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 5173,
-  },
-  // ✅ Add this to fix 404 errors on page reload (SPA fallback)
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  plugins: [react(), tailwindcss(), ],
 })
+
