d93edf180dc94fc7032c93fbddcd43ec8aa1cefd
Vansh Thapar <vanshthapar.professional@gmail.com>
2025-11-05 23:09:59 +0530

    Dockerized my app

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,6 @@ app.use('/',router);
 app.use('/',laundaryrouter);
 app.use('/',homeLaundary);
 
-app.listen(port,()=>{
+app.listen(port,"0.0.0.0",()=>{
     console.log(`http://localhost:${port}`);
 })
