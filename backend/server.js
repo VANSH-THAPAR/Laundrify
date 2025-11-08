327c4aa503db6babbfc065d61661555e7818554f
Vansh Thapar <vanshthapar.professional@gmail.com>
2025-11-08 14:01:52 +0530

    fixed cors policy error

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ connectDb();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Your frontend URL
+    origin: ["http://localhost:5173", "https://laundrify-vansh.netlify.app"], // Your frontend URL
     methods: ["GET", "POST"]
   }
 });
