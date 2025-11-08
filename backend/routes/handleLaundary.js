bdee008d574ab24e22c1b3786182219c85fcc69b
Vansh Thapar <vanshthapar.professional@gmail.com>
2025-11-08 11:02:50 +0530

    fixed a bug of laundry schema not defined

--- backend/routes/handleLaundary.js
+++ backend/routes/handleLaundary.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const loginSchema = require('../models/laundary');
+const laundarySchema = require('../models/laundary');
 const jwt = require('jsonwebtoken');
 const checkJwt = require('../middleware/checkJWT')
 require('dotenv').config();
